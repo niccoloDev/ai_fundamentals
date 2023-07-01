2fca04eb8d143b24b4ea03bac345babe6e9bf661
Niccolò Califano <ncalifano@local.com>
2023-07-01 13:29:15 +0200

    added argument to allow flask app to be available from remote machines and to work properly in container

--- run_app.py
+++ run_app.py
@@ -2,6 +2,6 @@ from webapp import app, socketio
 
 # Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host="0.0.0.0")
     # app.run(debug=True)
     
